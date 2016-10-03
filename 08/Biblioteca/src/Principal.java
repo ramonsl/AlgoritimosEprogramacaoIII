ad563771a008ab8c7620c7c2a42266517f9fa679
ramonsl <ramonsl@gmail.com>
2016-10-03 20:58:05 -0300

    Revisao
    Aula de revisão

--- 08/Biblioteca/src/Principal.java
+++ 08/Biblioteca/src/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
     public static void main(String[] args) {
     int op=0;
         Biblioteca minhaBiblioteca = new Biblioteca();
+        minhaBiblioteca.add();
         do{
             ///menu maroto
 
